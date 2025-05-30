15f38a1f573f7a702c8329d7695e52bd22fc954e
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2025-05-30 13:01:17 -0500

    feat: just adding some typing

--- remove_build_files.py
+++ remove_build_files.py
@@ -2,13 +2,16 @@ import os
 import fnmatch
 import shutil
 
+
 def read_gitignore(filepath='.gitignore'):
     with open(filepath, 'r') as file:
         patterns = file.readlines()
-    patterns = [pattern.strip() for pattern in patterns if pattern.strip() and not pattern.startswith('#')]
+    patterns = [pattern.strip() for pattern in patterns if pattern.strip()
+                and not pattern.startswith('#')]
     return patterns
 
-def matches_pattern(file_path, patterns, exceptions):
+
+def matches_pattern(file_path: str, patterns: list[str], exceptions: list[str]):
     for pattern in patterns:
         if any(fnmatch.fnmatch(file_path, exception) for exception in exceptions):
             return False
@@ -16,18 +19,21 @@ def matches_pattern(file_path, patterns, exceptions):
             return True
     return False
 
-def delete_files(base_path, patterns, exceptions):
+
+def delete_files(base_path: str, patterns: list[str], exceptions: list[str]):
     for root, dirs, files in os.walk(base_path, topdown=False):
         for name in files:
             file_path = os.path.relpath(os.path.join(root, name), base_path)
             if matches_pattern(file_path, patterns, exceptions):
                 os.remove(os.path.join(root, name))
                 print(f"Deleted file: {file_path}")
+
         for name in dirs:
             dir_path = os.path.relpath(os.path.join(root, name), base_path)
             if matches_pattern(dir_path, patterns, exceptions):
                 shutil.rmtree(os.path.join(root, name))
                 print(f"Deleted directory: {dir_path}")
+
 
 if __name__ == '__main__':
     gitignore_path = '.gitignore'  # Path to the .gitignore file
@@ -36,4 +42,3 @@ if __name__ == '__main__':
 
     patterns = read_gitignore(gitignore_path)
     delete_files(base_path, patterns, exceptions)
-
